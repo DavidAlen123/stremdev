c3b817c617841f20916595d6219570be933398ac
DavidAlen123 <83995118+DavidAlen123@users.noreply.github.com>
2024-04-20 13:46:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 from pytz import UTC
-import local
+import locale
 
 # Set page configuration
 st.set_page_config(
@@ -48,10 +48,7 @@ df['Cancelled At'] = pd.to_datetime(df['Cancelled At'])
 
 df['Total Price'] = pd.to_numeric(df['Total Price'], errors='coerce')
 
-
-# Convert the Quantity column to numeric
-df['Quantity'] = pd.to_numeric(df['Quantity'], errors='coerce')
-
+df['Unit Price'] = pd.to_numeric(df['Unit Price'], errors='coerce')
 # Set the locale to Indian English
 locale.setlocale(locale.LC_NUMERIC, 'en_IN')
 
@@ -64,11 +61,6 @@ df['Total Price'] = df['Total Price'].apply(format_indian_format)
 df['Unit Price'] = df['Unit Price'].apply(format_indian_format)
 # Convert the Quantity column to numeric
 df['Quantity'] = pd.to_numeric(df['Quantity'], errors='coerce')
-
-
-
-
-
 st.write(df)
 
 # Drop rows with specific salesperson names
@@ -120,30 +112,10 @@ cancelled_orders = df_filtered[df_filtered["Cancelled At"].notnull()]
 non_cancelled_orders = df_filtered[df_filtered["Cancelled At"].isnull()]
 
 # Grouping the data by salesperson and summing up the total sales for each group
-cancelled_sales = cancelled_orders.groupby('Sales Person')['Total Price'].sum().reset_index()
-non_cancelled_sales = non_cancelled_orders.groupby('Sales Person')['Total Price'].sum().reset_index()
+sales_data = non_cancelled_orders.groupby('Sales Person')['Total Price'].sum().reset_index(name='Sales Amount')
 
-# Merge the two dataframes on 'Sales Person'
-merged_sales = pd.merge(cancelled_sales, non_cancelled_sales, on='Sales Person', suffixes=('_Cancelled', '_Non_Cancelled'))
-
-# Calculate total amount of canceled orders for each salesperson
-cancelled_orders_total_amount = cancelled_orders.groupby('Sales Person')['Total Price'].sum().reset_index(name='Total Amount Cancelled')
-
-# Calculate total number of cancelled orders for each salesperson
-cancelled_orders_count = cancelled_orders.groupby('Sales Person').size().reset_index(name='Total Orders Cancelled')
-
-# Calculate total amount of non-cancelled orders for each salesperson
-non_cancelled_orders_total_amount = non_cancelled_orders.groupby('Sales Person')['Total Price'].sum().reset_index(name='Sales Amount')
-
-# Calculate total number of non-cancelled orders for each salesperson
-non_cancelled_orders_count = non_cancelled_orders.groupby('Sales Person').size().reset_index(name='Total Sales')
-
-# Merge total amount and total number of Sales orders
-sale_data = non_cancelled_orders_count.merge(non_cancelled_orders_total_amount, on='Sales Person')
-
-# Merge total amt and total number of cancel order
-cancel_data = cancelled_orders_count.merge(cancelled_orders_total_amount, on='Sales Person')
-
+# Grouping the data by salesperson and summing up the total cancelled amount for each group
+cancel_data = cancelled_orders.groupby('Sales Person')['Total Price'].sum().reset_index(name='Cancelled Amount')
 
 # Display non-cancelled order details
 st.subheader("Sales Order Details:")
@@ -157,7 +129,7 @@ col1,col2 = st.columns(2)
 with col1:
     # Display the Sales data
     st.subheader("Sales Order Summary:")
-    st.write(sale_data)
+    st.write(sales_data)
 with col2:
     # Display the Cancel data
     st.subheader("Cancelled Order Summary:")
@@ -165,60 +137,52 @@ with col2:
 
 
 # Calculate total price for each selected SKU
-total_price_per_sku = df_filtered.groupby('SKU')['Total Price'].sum().reset_index(name='Total Price')
+# Filter out cancelled orders from the original DataFrame
+df_filtered['Total Price'] = pd.to_numeric(df_filtered['Total Price'], errors='coerce')
+df_filtered_non_cancelled = df_filtered[df_filtered['Cancelled At'].isnull()]
 
+# Group the data by SKU and sum up the total price and quantity for each SKU
+sku_summary = df_filtered_non_cancelled.groupby('SKU').agg({'Total Price': 'sum', 'Quantity': 'sum', 'Title': 'first', 'Vendor': 'first','Created At':'first','Cancelled At':'first'}).reset_index()
 
 # Add a dropdown widget for selecting the number of top SKUs
 top_sku_count = st.sidebar.selectbox("Select the Number of Top SKUs", [10, 50, 100, 150, 200])
-
-# Sort the grouped DataFrame in descending order based on total sales
-top_sku_sales = total_price_per_sku.sort_values(by='Total Price', ascending=False).head(top_sku_count)
-
-
-# Group the data by SKU and sum up the total price and quantity for each SKU
-sku_summary = df_filtered.groupby('SKU').agg({'Total Price': 'sum', 'Quantity': 'sum', 'Title': 'first', 'Vendor': 'first','Created At':'first'}).reset_index()
 
 # Sort the grouped DataFrame in descending order based on total sales
 top_sku_sales = sku_summary.sort_values(by='Total Price', ascending=False).head(top_sku_count)
 
 # Display the top SKUs with quantity, title, vendor, and total price
 st.subheader(f"Top {top_sku_count} SKUs with Highest Sales:")
-st.write(top_sku_sales[['SKU','Created At','Title', 'Vendor', 'Quantity', 'Total Price']])
-
+st.write(top_sku_sales[['SKU','Quantity', 'Total Price']])
 
 #####################################################################3
 
+# Calculate total price for each selected SKU from non-cancelled orders
+vendor_wise_sale_total = df_filtered_non_cancelled.groupby('Vendor')['Total Price'].sum().reset_index(name='Total Price')
 
-# Calculate total price for each selected SKU
-vendor_wise_sale_total = df_filtered.groupby('Vendor')['Total Price'].sum().reset_index(name='Total Price')
-
-
-# Add a dropdown widget for selecting the number of top SKUs
-top_vendor_count = st.sidebar.selectbox("Select the Number of Top Vendor's", [10, 50, 100, 150, 200])
+# Add a dropdown widget for selecting the number of top vendors
+top_vendor_count = st.sidebar.selectbox("Select the Number of Top Vendors", [10, 50, 100, 150, 200])
 
 # Sort the grouped DataFrame in descending order based on total sales
 top_vendor_sales = vendor_wise_sale_total.sort_values(by='Total Price', ascending=False).head(top_vendor_count)
 
-# Group the data by SKU and sum up the total price and quantity for each SKU
-vendor_summary = df_filtered.groupby('Vendor').agg({'Total Price': 'sum', 'Quantity': 'sum', 'Title': 'first','SKU':'first', 'Created At':'first' }).reset_index()
+# Group the data by vendor and retrieve other relevant information
+vendor_summary = df_filtered_non_cancelled.groupby('Vendor').agg({'Total Price': 'sum', 'Quantity': 'sum', 'Title': 'first', 'SKU': 'first', 'Created At': 'first', 'Cancelled At': 'first'}).reset_index()
 
 # Sort the grouped DataFrame in descending order based on total sales
 top_vendor_sales = vendor_summary.sort_values(by='Total Price', ascending=False).head(top_vendor_count)
 
-# Display the top SKUs with quantity, title, vendor, and total price
-st.subheader(f"Top {top_vendor_count} Vendor's with Highest Sales:")
-st.write(top_vendor_sales[['Vendor', 'Created At','Title','SKU', 'Quantity', 'Total Price']])
-
-
+# Display the top vendors with quantity, title, SKU, total price, and cancelled at information
+st.subheader(f"Top {top_vendor_count} Vendors with Highest Sales:")
+st.write(top_vendor_sales[['Vendor', 'Title', 'Total Price']])
 
 import plotly.express as px
 
 # Visualize Sales Order Summary
-fig1 = px.bar(sale_data, x='Sales Person', y='Sales Amount', title='Sales Amount by Sales Person')
+fig1 = px.bar(sales_data, x='Sales Person', y='Sales Amount', title='Sales Amount by Sales Person')
 st.plotly_chart(fig1)
 
 # Visualize Cancelled Order Summary
-fig2 = px.bar(cancel_data, x='Sales Person', y='Total Amount Cancelled', title='Cancelled Amount by Sales Person')
+fig2 = px.bar(cancel_data, x='Sales Person', y='Cancelled Amount', title='Cancelled Amount by Sales Person')
 st.plotly_chart(fig2)
 
 # Visualize Top SKUs with Highest Sales
@@ -228,26 +192,3 @@ st.plotly_chart(fig3)
 # Visualize Top Vendors with Highest Sales
 fig4 = px.bar(top_vendor_sales, x='Vendor', y='Total Price', title=f'Top {top_vendor_count} Vendors with Highest Sales')
 st.plotly_chart(fig4)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
